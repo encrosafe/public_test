6ff1574d36951c8a12b59a4f49c1129532c226fc
Alfredo0815 <alfred.schuetz@pustohl.de>
2021-12-23 14:29:58 +0100

    fix import urls.py

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from server.core import views as core
+from core import views as core
 
 
 urlpatterns = [
