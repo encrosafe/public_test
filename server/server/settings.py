2204dc09bf59e72efff918c4159945e71d3e46a9
Alfredo0815 <alfred.schuetz@pustohl.de>
2021-12-28 20:45:12 +0100

    fix 4.2: added media dir, corrected templates

--- server/server/settings.py
+++ server/server/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/static/medias/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
